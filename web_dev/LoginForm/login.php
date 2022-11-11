f81fad4bc26200b76239beea5b52b98f588c560b
MiniMECHlg <lucy.gardener2004@gmail.com>
2022-11-11 15:23:45 +0000

    Started connecting a server

--- web_dev/LoginForm/login.php
+++ web_dev/LoginForm/login.php
@@ -1,5 +1,15 @@
 <?php
 
+$username = "WS328700_admin";
+$password = "HTML5ncss3!";
+$db_name = "WS328700_DD";
+$server = "plesk.remote.ac";
+
+$db_connect = mysqli_connect($server, $username, $password, $db_name)
+
+
+
+/*
 if(empty($_POST['txtUsername']))
 {
     die("A username is required");
@@ -15,5 +25,5 @@ $_SESSION['txtUsername'] = $_POST['txtUsername'];
 $_SESSION['txtPassword'] = $_POST['txtPassword'];
 
 header("Location: ./");
-
+*/ 
 ?>
